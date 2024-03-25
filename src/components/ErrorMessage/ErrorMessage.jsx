aa795ca371388714a84af60e34c451c67c3e4050
Your Name <kadry.nukr@ukr.net>
2024-03-25 16:36:40 +0200

    begin

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,9 +0,0 @@
-import css from './ErrorMessage.module.css'
-function ErrorMessage (){
-    return(
-        <>
-        <p className={css.text}>Yuo have a problem, reboot page</p>
-        </>
-    );
-}
-export default ErrorMessage;
